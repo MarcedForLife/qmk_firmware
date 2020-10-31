5725ff835a41075c1cf74be31923857404afe5f2
Marc Tucker <marcedforlife@gmail.com>
2020-11-01 00:18:53 +1300

    Added initial tap and hold support

--- keyboards/dztech/dz65rgb/keymaps/marcedforlife/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/marcedforlife/keymap.c
@@ -2,7 +2,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi(
-        KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
+        KC_ESC,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
         KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,
         CTL_T(KC_CAPS), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_DEL,
         KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_MPLY,
@@ -16,3 +16,57 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,        _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
     )
 };
+
+/* Timers for numberic keys for detecting hold events
+static uint16_t fn_key_timer[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
+*/
+static uint16_t esc_key_timer = -1;
+
+/**
+ * Listens for user input events
+ */
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode){
+        case KC_ESC: {
+            if (record->event.pressed) {
+                esc_key_timer = timer_read();
+            } else if (esc_key_timer != -1) {
+                esc_key_timer = -1;
+                tap_code(KC_ESC);
+            }
+            return false;
+        }
+        /* Disabled numeric F keys on hold
+        case KC_1 ... KC_0: {
+            uint8_t index = keycode - KC_1;
+            if (record->event.pressed) {
+                fn_key_timer[index] = timer_read();
+            } else if (fn_key_timer[index] != -1) {
+                fn_key_timer[index] = -1;
+                tap_code(KC_1 + index);
+            }
+            return false;
+        }
+        */
+    }
+    return true;
+}
+
+/**
+ * Should avoid using this but it allows sending hold key events without releasing the held key
+ */
+void matrix_scan_user(void) {
+    if(esc_key_timer != -1 && timer_elapsed(esc_key_timer) > TAPPING_TERM) {
+        esc_key_timer = -1;
+        tap_code(KC_GRV);
+    }
+    /* Disabled numeric F keys on hold
+    int index;
+    for (index = 0; index < 10; index++) {
+        if(fn_key_timer[index] != -1 && timer_elapsed(fn_key_timer[index]) > TAPPING_TERM) {
+            fn_key_timer[index] = -1;
+            tap_code(KC_F1 + index);
+        }
+    }
+    */
+}
